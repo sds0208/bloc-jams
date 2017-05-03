5737fd0e20a1a4f00719a2ed21ea52c782297325
Sarah Santa Cruz <Sarah Santa Cruz>
2017-05-03 18:47:53 -0500

    checkpoint 9 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,31 @@
+var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function() {
+var animatePoints = function(points)  {
 
-    var points = document.getElementsByClassName('point');
+  var revealPoint = function() {
 
-    for ( var i = 0; i < points.length; i++ ) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
+        for ( var i = 0; i < pointsArray.length; i++ ) {
+          pointsArray[i].style.opacity = 1;
+          pointsArray[i].style.transform = "scaleX(1) translateY(0)";
+          pointsArray[i].style.msTransform = "scaleX(1) translateY(0)";
+          pointsArray[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+  };
+    revealPoint();
 };
 
-revealPoint();
+
+window.onload = function()  {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event)  {
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+          animatePoints(pointsArray);
+      }
+
+    });
+}
