960fccfb2f66540473d0edf90b86e9dcef648e7a
Sarah Santa Cruz <Sarah Santa Cruz>
2017-05-21 19:50:32 -0500

    checkpoint 16 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,27 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points)  {
+var animatePoints = function()  {
 
   var revealPoint = function() {
 
-        for ( var i = 0; i < pointsArray.length; i++ ) {
-          pointsArray[i].style.opacity = 1;
-          pointsArray[i].style.transform = "scaleX(1) translateY(0)";
-          pointsArray[i].style.msTransform = "scaleX(1) translateY(0)";
-          pointsArray[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+      $(this).css({
+          opacity: 1,
+          transform: 'scaleX(1) translateY(0)'
+      });
   };
-    revealPoint();
+  $.each($('.point'), revealPoint);
 };
 
-
-window.onload = function()  {
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
+$(window).load(function() {
+    if ($(window).height() > 950) {
+        animatePoints();
     }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-    window.addEventListener('scroll', function(event)  {
-      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-          animatePoints(pointsArray);
-      }
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+    $(window).scroll(function(event)  {
+        if ($(window).scrollTop() >= scrollDistance)  {
+            animatePoints();
+        }
+
 
     });
-}
+});
