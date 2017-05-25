73e25de0fa66bae0745f8c8ecf31398aab029ea2
Sarah Santa Cruz <Sarah Santa Cruz>
2017-05-24 19:40:56 -0500

    checkpoint 17 completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,27 +1,33 @@
-var collectionItemTemplate =
-    '<div class="collection-album-container column fourth">'
-  + '  <img src="assets/images/album_covers/01.png"/>'
-  + '  <div class="collection-album-info caption">'
-  + '    <p>'
-  + '      <a class="album-name" href="album.html"> The Colors </a>'
-  + '      <br/>'
-  + '      <a href="album.html"> Pablo Picasso </a>'
-  + '      <br/>'
-  + '      X songs'
-  + '      <br/>'
-  + '    </p>'
-  + '  </div>'
-  + '</div>'
-  ;
+var buildCollectionItemTemplate = function()  {
+    var template =
+      '<div class="collection-album-container column fourth">'
+    + '  <img src="assets/images/album_covers/01.png"/>'
+    + '  <div class="collection-album-info caption">'
+    + '    <p>'
+    + '      <a class="album-name" href="album.html"> The Colors </a>'
+    + '      <br/>'
+    + '      <a href="album.html"> Pablo Picasso </a>'
+    + '      <br/>'
+    + '      X songs'
+    + '      <br/>'
+    + '    </p>'
+    + '  </div>'
+    + '</div>'
+      ;
 
-  window.onload = function() {
+    return $(template);
+};
+
+  $(window).load(function() {
       // #1
-      var collectionContainer = document.getElementsByClassName('album-covers')[0];
+      var $collectionContainer = $('.album-covers');
       // #2
-      collectionContainer.innerHTML = '';
+      $collectionContainer.empty();
 
       // #3
       for (var i = 0; i < 12; i++) {
-          collectionContainer.innerHTML += collectionItemTemplate;
+          var $newThumbnail = buildCollectionItemTemplate();
+
+          $collectionContainer.append($newThumbnail);
       }
-  }
+  });
